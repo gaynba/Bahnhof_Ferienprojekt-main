794db973415e4b5501ab4ee4945eeb6d53be7414
Jordan-Velte <jordan.velte@gymhum.de>
2022-05-13 09:04:48 +0200

    buchung

---
+++ Bahnhof_Ferienprojekt/src/main/java/com/example/Bahnhof_Ferienprojekt/models/Buchung.java
@@ -0,0 +1,78 @@
+package com.example.Bahnhof_Ferienprojekt.models;
+
+public class Buchung {
+    int id;
+    String passagier;
+    int passagierId;
+    String zuglinie;
+    int zuglinieId;
+    int buchungsnummer;
+    //In Euro
+    int preis;
+    String buchungsdatum;
+    //ArrayList für ausgewählte Bahnhöfe!
+
+    //CONSTRUCTOR
+    public Buchung(int id, int buchungsnummer, String zuglinie, int zuglinieId, String passagier, int passagierId, int preis, String buchungsdatum){
+        setId(id);
+        setBuchungsnummer(buchungsnummer);
+        setZuglinie(zuglinie);
+        setZuglinieId(zuglinieId);
+        setPassagier(passagier);
+        setPassagierId(passagierId);
+        setPreis(preis);
+        setBuchungsdatum(buchungsdatum);
+    }
+
+    //Setter & Getter
+    public void setBuchungsnummer(int buchungsnummer) {
+        this.buchungsnummer = buchungsnummer;
+    }
+    public void setBuchungsdatum(String buchungsdatum) {
+        this.buchungsdatum = buchungsdatum;
+    }
+    public void setPassagier(String passagier) {
+        this.passagier = passagier;
+    }
+    public void setPassagierId(int passagierId) {
+        this.passagierId = passagierId;
+    }
+    public void setZuglinie(String zuglinie) {
+        this.zuglinie = zuglinie;
+    }
+    public void setZuglinieId(int zuglinieId) {
+        this.zuglinieId = zuglinieId;
+    }
+    public void setPreis(int preis) {
+        this.preis = preis;
+    }
+    public int getBuchungsnummer() {
+        return buchungsnummer;
+    }
+    public String getBuchungsdatum() {
+        return buchungsdatum;
+    }
+    public int getPreis() {
+        return preis;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getPassagier() {
+        return passagier;
+    }
+    public int getPassagierId() {
+        return passagierId;
+    }
+    public String getZuglinie() {
+        return zuglinie;
+    }
+    public int getZuglinieId() {
+        return zuglinieId;
+    }
+
+
+}
